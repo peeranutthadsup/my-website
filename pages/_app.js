7c8715ec1a0825d693bcb1307d5fce43628ee64e
Peeranut Thadsup <peeranut.thadsp@gmail.com>
2025-06-09 03:27:21 +0700

    Remove unused globals.css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css'  // <-- เพิ่มบรรทัดนี้
-// import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
