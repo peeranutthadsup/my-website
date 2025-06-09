6c719cb01ea528dc020772bd4996f2244d79147b
Peeranut Thadsup <peeranut.thadsp@gmail.com>
2025-06-09 10:40:53 +0700

    update code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09b4909b520927dd"; }
+/******/ 		__webpack_require__.h = function() { return "5d8e16e4cbe9a451"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b6ad0245b80d3568"; }
+/******/ 	__webpack_require__.h = function() { return "5d8e16e4cbe9a451"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
