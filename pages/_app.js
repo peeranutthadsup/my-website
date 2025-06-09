269063cf18a57bb6f9ed9591c3f29bdd52660c0e
peeranutthadsup <79188346+peeranutthadsup@users.noreply.github.com>
2025-06-09 10:07:58 +0700

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-import 'bootstrap/dist/css/bootstrap.min.css'  // <-- เพิ่มบรรทัดนี้
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
